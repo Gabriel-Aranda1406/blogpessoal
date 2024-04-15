303a383d9185ffa0e4f43f364751dd2981a3ea97
Gabriel-Aranda1406 <biel.aranda234@gmail.com>
2024-04-15 13:08:35 -0300

    Blog-Pessoal__update__comment

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe()); //U
 
-  app.enableCors(); //Essencial pro Front-End funcionar,serve para aceitar requisições de outras origens, não apenas do próprio servidor onde está.No mundo empresarial, se coloca o endereço nos parenteses para limitar o acesso.
+  app.enableCors(); //Essencial pro Front-End funcionar,serve para aceitar requisições de outras origens, não apenas do próprio servidor onde está. No mundo empresarial, se coloca o endereço nos parenteses para limitar o acesso.
 
   await app.listen(4000);
 }
