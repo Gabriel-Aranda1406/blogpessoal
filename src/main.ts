113a743778f600606b4f80742c877e5aa459d7c3
Gabriel-Aranda1406 <biel.aranda234@gmail.com>
2024-04-15 11:41:58 -0300

    Blog-Pessoal - Listar Postagens

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  process.env.TZ = '-03:00'; //Ajuste de fuso horário pro Brasil
+
+  app.useGlobalPipes(new ValidationPipe()); //U
+
+  app.enableCors(); //Essencial pro Front-End funcionar,serve para aceitar requisições de outras origens, não apenas do próprio servidor onde está.No mundo empresarial, se coloca o enderçeo nos parenteses para limitar o acesso.
+
+  await app.listen(4000);
+}
+bootstrap();
